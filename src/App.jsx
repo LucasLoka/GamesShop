33e69128f5556a4af2774c34e7a42c14ef4cb21a
LucasLoka <lucaslou835@gmail.com>
2024-11-18 17:27:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1> GAMESHOP </h1>
+        <p>Seu Lugar de comprar games barato!</p>
+
+        <div>
+          <h1>Jogos:  </h1>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
